3b7a78a8616a779e082ef15ab1287afe8963db70
JuanDavidSerna <jserna@cloudlabs.co>
2024-06-08 10:45:53 -0500

    PracticeJPA

---
+++ src/main/java/org/juandavid/springboot/jpa/practicespringbootjpa/PracticeSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package org.juandavid.springboot.jpa.practicespringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/juandavid/springboot/jpa/practicespringbootjpa/PracticeSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.juandavid.springboot.jpa.practicespringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
